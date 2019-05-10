4ed11ced56960db750929ddb3b12f4319af1f90e
Sören Kohnert <soeren.kohnert.ext@siemens.com>
2019-05-10 18:47:35 +0200

    generic Comment section

--- functions/openDev.m
+++ functions/openDev.m
@@ -1,12 +1,27 @@
 function obj=openDev(obj)
-%obj=openDev(obj) opens some stuff
-%   
-%obj=openDev(obj)
+% Opens connection to Device.
+% 
+% Open function for interfaces, independent of which interface type is 
+% used. Reads the obj.mode property to decide which low level open 
+% function is used to fill the obj.commHandle. 
 %
-%   See also readDev,
-%   Copyright 2019-2022 The BohnertAletseeWorks, Inc.
-%   References:
-%     Nix
+% Parameters:
+%  obj:         labDevice Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% Return values:
+%  obj:         lab Device Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% See also: closeDev, writeDev, readDev, closeDev, flushDev, rwDev
+% 
+% References: 
 
 
 end
