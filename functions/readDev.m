345a230e1d2ac7f65805e804e7c48c46731ae44b
Sören Kohnert <soeren.kohnert.ext@siemens.com>
2019-05-10 19:09:28 +0200

    Comments Added

--- functions/readDev.m
+++ functions/readDev.m
@@ -1,4 +1,26 @@
 function readDev(object)
-
+% Reads data from Device.
+% 
+% Read function for interfaces, independent of which interface type is 
+% used. Reads the obj.mode property to decide which low level read 
+% function is used on the obj.commHandle. 
+%
+% Parameters:
+%  obj:         labDevice Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% Return values:
+%  obj:         lab Device Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% See also: closeDev, writeDev, openDev, closeDev, flushDev, rwDev
+% 
+% References: 
 
 return ret
--- functions/writeDev.m
+++ functions/writeDev.m
@@ -1,4 +1,26 @@
 function writeDev(object,txt)
-disp('ich bin wirte')
+% Writes string to Device.
+% 
+% Write function for interfaces, independent of which interface type is 
+% used. Reads the obj.mode property to decide which low level write 
+% function is used on the obj.commHandle. 
+%
+% Parameters:
+%  obj:         labDevice Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% Return values:
+%  obj:         lab Device Handle with properties
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%
+% See also: closeDev, readDev, openDev, closeDev, flushDev, rwDev
+% 
+% References: 
 
 return 
