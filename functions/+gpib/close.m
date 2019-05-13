bd1c01bb5fc197273648ec5a3f0061b2410d74b8
emv <emv@hs-augsburg.de>
2019-05-13 17:45:12 +0200

    filled gpib close,open,write

--- functions/+gpib/close.m
+++ functions/+gpib/close.m
@@ -1,5 +1,34 @@
 function obj=close(obj)
-fclose(obj.prop.commHandle.handle);
+% Closes a GPIB connection.
+% 
+% Closes a VISA (gpib) interfaces. 
+%  
+%
+%  obj:         labDevice Handle with properties
+%               obj.prop.
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%                - gpib.primaryAdress
+%                - gpib.vendor
+%                - gpib.boardIndex
+%
+%
+% Return values:
+%  obj:         lab Device Handle with properties
+%               obj.prop.
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%                - gpib.primaryAdress
+%                - gpib.boardIndex
+%
+% See also: gpib.open, gpib.write, gpib.read
+%
+% References:
 
+fclose(obj.prop.commHandle);
 
-end
+return
--- functions/+gpib/write.m
+++ functions/+gpib/write.m
@@ -1,5 +1,28 @@
 function ret=write(obj,txt)
+% Closes a GPIB connection.
+% 
+% Closes a VISA (gpib) interfaces. 
+%  
+%
+%  obj:         labDevice Handle with properties
+%               obj.prop.
+%                - mode
+%                - address
+%                - port
+%                - comm(unication)Handle (interface specific)
+%                - gpib.primaryAdress
+%                - gpib.vendor
+%                - gpib.boardIndex
+%
+%
+% Return values:
+%  ret:         1 if it was successfull.
+%
+% See also: gpib.open, gpib.close, gpib.read
+%
+% References:
 
-fprintf(obj.prop.commHandle,'%s\r',txt);
-ret=0;
+fprintf(obj.prop.commHandle,txt);
+
+ret=1;
 return
