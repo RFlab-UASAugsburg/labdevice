39ed947465ce16c459c02dae3787ac2b0110c50a
emv <emv@hs-augsburg.de>
2019-07-17 14:12:52 +0200

    Codeformatierung

--- @SMX/setFreq.m
+++ @SMX/setFreq.m
@@ -1,8 +1,8 @@
-function setFreq(obj,freq)
+function setFreq(obj, freq)
 
-writeDev(obj,['RF ',num2str(freq)]);
+writeDev(obj, ['RF ', num2str(freq)]);
 
-% result='000000';
+% result = '000000';
 % % Abfrage des Operation Complete Bit
 % while str2num(result(6)) ~= 1
 %     writeDev(obj, '*OPC?');
