8d695de16872bb47fb64000f9a3f5cd1c90367f3
emv <emv@hs-augsburg.de>
2019-07-17 13:41:16 +0200

    Test

--- @SMX/SMX.m
+++ @SMX/SMX.m
@@ -3,8 +3,8 @@ classdef  SMX < labDevice
       % Add device specific properties here
     end
    methods 
-       function obj=SMX(mode,varargin)
-           obj@labDevice(mode,varargin);
+       function obj = SMX(mode, varargin)
+           obj@labDevice(mode, varargin);
            % add device specific constructor from here on
        end
        
@@ -16,8 +16,8 @@ classdef  SMX < labDevice
        % All functions that are inheritanced methods of the superclass
        % labDevice need to be initalised here once more.
        
-       function ret=read(obj)
-           ret=read@labDevice(obj);
+       function ret = read(obj)
+           ret = read@labDevice(obj);
            % add device specific read from here on
        end
        
