ee334c45080d4d8181836873da3214ffc553bf54
Daniel Bauer <daniel.bauer@hs-augsburg.de>
2019-09-11 15:50:22 +0200

    GUI update. Surface update. Working on callbacks.

--- scripts/scriptsGUI/InitializeESRPConnectionGUI.m
+++ scripts/scriptsGUI/InitializeESRPConnectionGUI.m
@@ -1,4 +1,4 @@
-function InitializeESRPConnectionGUI(connection)
+function objESRP = InitializeESRPConnectionGUI(connection)
 %
 % Establish the connection w/ the ESRP
 %
@@ -28,4 +28,5 @@ switch connection
 
 end
 
+return objESRP
 end
