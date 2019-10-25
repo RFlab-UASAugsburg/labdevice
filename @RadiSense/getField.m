521b46e84ce703f2a6439441f5305062ed1c97fc
emv <emv@hs-augsburg.de>
2019-10-25 13:22:37 +0200

    implement absolute value reading and error handling

--- @RadiSense/getField.m
+++ @RadiSense/getField.m
@@ -1,18 +1,27 @@
 function ret = getField(obj)
-%GETFIELD Summary of this function goes here
-%   Detailed explanation goes here
-write(obj, "D2");
-response = read(obj);
-if length(response) ~= 18
-    fprintf("something went wrong: " + response);
-    ret = 1;
-    return;
+%GETFIELD get averaged value of all axis
+%
+%   returns absolute value of the electric field vector, does three queries
+%   since we don't know what the sensor does when we request a reading of
+%   all axis at once.
+messages = ["AEDD", "ADED", "ADDE"];
+axis = [0,0,0];
+for i = 1:3
+    write(obj,messages(i));
+    read(obj);
+    write(obj,"D2");
+    response = read(obj);
+    if length(response) ~= 18
+        msgID = "RADISENSE:WeirdError";
+        msg = "something went wrong: " + response;
+        throw(MException(msgID,msg));
+    elseif response(14) == "O"
+        msgID = "RADISENSE:OverRange";
+        msg = "Probe returned over range warning!";
+        throw(MException(msgID,msg));
+    end
+    axis(i) = double(string(extractBetween(response,3,7)));
 end
-if response(14) == O
-    fprintf("Over range!");
-    ret = 1;
-    return;
-end
-ret = extractBetween(response,3,7);
+ret = sqrt(axis(1).^2 + axis(2).^2 + axis(3).^2);
 end
 
