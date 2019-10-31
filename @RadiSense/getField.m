bafb5f41d749c55ae6dbcbcea19a8e59acf3dd68
emv <emv@hs-augsburg.de>
2019-10-31 17:32:02 +0100

    Simplyfied getField: the sensor already returns the euclidian norm of all enabled axis', so doing it ourselves isn't necessary.

--- @RadiSense/getField.m
+++ @RadiSense/getField.m
@@ -1,27 +1,20 @@
 function ret = getField(obj)
 %GETFIELD get averaged value of all axis
 %
-%   returns absolute value of the electric field vector, does three queries
-%   since we don't know what the sensor does when we request a reading of
-%   all axis at once.
-messages = ["AEDD", "ADED", "ADDE"];
-axis = [0,0,0];
-for i = 1:3
-    write(obj,messages(i));
-    read(obj);
-    write(obj,"D2");
-    response = read(obj);
-    if length(response) ~= 18
-        msgID = "RADISENSE:WeirdError";
-        msg = "something went wrong: " + response;
-        throw(MException(msgID,msg));
-    elseif response(14) == "O"
-        msgID = "RADISENSE:OverRange";
-        msg = "Probe returned over range warning!";
-        throw(MException(msgID,msg));
-    end
-    axis(i) = double(string(extractBetween(response,3,7)));
+%   enable all axis, extract the measurement from returned data. The sensor
+%   returns the euclidian norm of all enabled axis.
+obj.write("AEEE");
+obj.read;
+obj.write("D2");
+response = read(obj);
+if length(response) ~= 18
+    msgID = "RADISENSE:WeirdError";
+    msg = "something went wrong: " + response;
+    throw(MException(msgID,msg));
+elseif response(14) == "O"
+    msgID = "RADISENSE:OverRange";
+    msg = "Probe returned over range warning!";
+    throw(MException(msgID,msg));
 end
-ret = sqrt(axis(1).^2 + axis(2).^2 + axis(3).^2);
+ret = double(string(extractBetween(response,3,7)));
 end
-
