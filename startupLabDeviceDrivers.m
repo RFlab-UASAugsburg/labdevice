1e1a0b3666d58743186e703aae2c19d8f83cbe31
emv <emv@hs-augsburg.de>
2019-11-04 20:51:37 +0100

    startup skript

---
+++ startupLabDeviceDrivers.m
@@ -0,0 +1,8 @@
+function startupLabDeviceDrivers()
+    disp('Run Initialization for LabDeviceDrivers... ')
+    projectRootDir = fileparts(mfilename('fullpath'));
+
+    addpath(fullfile(projectRootDir, 'cfg'))
+    addpath(fullfile(projectRootDir, 'functions'))
+
+end
