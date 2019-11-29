27a1a225877297d1f7c738480c9e65f767874b7a
Thomas Homm <thomas.homm@hs-augsburg.de>
2019-11-29 14:39:28 +0100

    Update read.m to reduce errors when reading a lot of data at once

--- functions/+customEthOverJava/read.m
+++ functions/+customEthOverJava/read.m
@@ -14,17 +14,21 @@ function message = read(obj)
             % get a buffered data input stream from the socket
             input_stream   = input_socket.getInputStream;
             d_input_stream = DataInputStream(input_stream);
-
-            fprintf(1, 'Connected to server\n');
-
             % read data from the socket - wait a short time first
-            pause(0.5);
+            i = tic;
             bytes_available = input_stream.available;
-            fprintf(1, 'Reading %d bytes\n', bytes_available);
+            while bytes_available == 0 && toc(i) < 1
+                pause(0.1);
+                bytes_available = input_stream.available;
+            end
                         
             data_reader = DataReader(d_input_stream);
             message = data_reader.readBuffer(bytes_available);
             message = char(message');
+            bytes_available = input_stream.available;
+            if bytes_available ~= 0
+                message = [message read(obj)];
+            end
             break;
         catch aexception
             s = aexception.message;
