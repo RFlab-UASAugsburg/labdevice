61f7beb101b106c6e6b516703e729760ce4f8334
thomas <thomas.homm@hs-augsburg.de>
2020-08-20 14:15:24 +0200

    fix: add read loop to increase reliability

--- @ESRP/getTrace.m
+++ @ESRP/getTrace.m
@@ -5,12 +5,13 @@
 %> returns a double array of trace values
 %>
 %> @param obj Instance of class
+%> @param freqPoints number of frequency points in the trace
 %> @param varargin select the trace index, defaults to 1
 %>
 %> @return stepsize in Hz
 % =====================================================
 
-function trace = getTrace(obj, varargin)
+function trace = getTrace(obj, freqPoints, varargin)
     if ~isempty(varargin)
         trac_num = varargin{1};
     else
@@ -18,15 +19,24 @@ function trace = getTrace(obj, varargin)
     end
     prev = obj.read; %empty buffer in case data was previously requested and not read
     obj.write("TRAC? TRACE" + trac_num + "; *WAI");
+    pause(0.1);
     message = obj.read;
-    trace = sscanf(message,"%e,");
-    if isempty(trace)
-        if isempty(message)
-            error("no answer from device");
-        else
-            disp(prev);
-            disp(message);
-            error("bad message from device");
+    i=0;
+    %make sure we've read all the data available
+    while length(sscanf(message,'%f,')) < freqPoints
+        if i > 5
+            error("not enough data from ESRP");
         end
+        pause(0.1)
+        message = [data obj.read];
+        i = i+1;
+    end
+    %parse read data
+    trace = sscanf(message,"%e,");
+    %handle 
+    if isempty(trace)
+        disp(prev);
+        disp(message);
+        error("bad message from device");
     end
 end
