f004575e352c5e1650062ede32904b12e9087074
Stefan Lammel <stefan.lammel@hs-augsburg.de>
2020-09-01 10:07:57 +0200

    refact: changed variable name for easier understandability

--- @RTM3000/defineMathChannelOperation.m
+++ @RTM3000/defineMathChannelOperation.m
@@ -3,7 +3,7 @@
 %>
 %> @param obj Instance of class
 %>
-%> @param waveform selected math waveform
+%> @param waveform_nr selected math waveform (e.g. MATH1 <- 1)
 %>
 %> @param varargin source channel(s) for the math operation
 %> Syntax: Channels: "CH1"
@@ -32,7 +32,7 @@
 %> 
 % =====================================================
 
-function defineMathChannelOperation(obj, waveform, operation, varargin)
+function defineMathChannelOperation(obj, waveform_nr, operation, varargin)
     operation = upper(operation);
     possibleOperations = {'ADD','SUB','MUL','DIV','SQR','SQRT','ABS','REC','INV','LOG','LN','DERI','INT','IIRL','IIRH','FFTMAG',};
     if any(strcmp(possibleOperations,operation))
@@ -41,40 +41,40 @@ function defineMathChannelOperation(obj, waveform, operation, varargin)
         elseif nargin > 4
             switch operation
                 case "ADD"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "ADD('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "ADD('+varargin{1}+","+varargin{2}+')"');
                 case "SUB"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "SUB('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "SUB('+varargin{1}+","+varargin{2}+')"');
                 case "MULT"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "MULT('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "MULT('+varargin{1}+","+varargin{2}+')"');
                 case "DIV"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "DIV('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "DIV('+varargin{1}+","+varargin{2}+')"');
                 case "IIRL"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "IIRL('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "IIRL('+varargin{1}+","+varargin{2}+')"');
                 case "IIRH"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "IIRH('+varargin{1}+","+varargin{2}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "IIRH('+varargin{1}+","+varargin{2}+')"');
             end
         elseif nargin > 3
             switch operation
                 case "SQR"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "SQR('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "SQR('+varargin{1}+')"');
                 case "SQRT"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "SQRT('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "SQRT('+varargin{1}+')"');
                 case "ABS"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "ABS('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "ABS('+varargin{1}+')"');
                 case "REC"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "REC('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "REC('+varargin{1}+')"');
                 case "INV"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "INV('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "INV('+varargin{1}+')"');
                 case "LOG"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "LOG('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "LOG('+varargin{1}+')"');
                 case "LN"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "LN('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "LN('+varargin{1}+')"');
                 case "DERI"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "DERI('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "DERI('+varargin{1}+')"');
                 case "INT"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "INT('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "INT('+varargin{1}+')"');
                 case "FFT"
-                    write(obj,"CALC:MATH"+waveform+':EXPR:DEF "FFTMAG('+varargin{1}+')"');
+                    write(obj,"CALC:MATH"+waveform_nr+':EXPR:DEF "FFTMAG('+varargin{1}+')"');
             end
         else
             error("not enough input arguments");
