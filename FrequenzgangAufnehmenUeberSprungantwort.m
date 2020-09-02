cab0f43264b62078efc0896882aebe302d991898
Stefan Lammel <stefan.lammel@hs-augsburg.de>
2020-09-02 13:45:46 +0200

    feat: script seems to work now

--- FrequenzgangAufnehmenUeberSprungantwort.m
+++ FrequenzgangAufnehmenUeberSprungantwort.m
@@ -7,6 +7,7 @@ Input_channel_nr = str2double(extractAfter(ChannelDUTInputSignal,"CH"));
 Output_channel_nr = str2double(extractAfter(ChannelDUTOutputSignal,"CH"));
 
 %% Signalgeneratoreinstellungen
+disp("Signalgenerator wird konfiguriert");
 % Channelkopplung der Signalgeneratorausgänge aktivieren
 EnableDisableChannelCoupling(Gen,"ON");
 % Signaltyp und Anfangswerte definieren
@@ -15,8 +16,10 @@ EnableDisableChannelCoupling(Gen,"ON");
 % Offset = Anfangsampl. * 1/2
 % Phase  = 0
 setSourceSquare(Gen,1,f_step,VPP_step,VPP_step/2,0);
+disp("Konfiguration des Signalgenerators abgeschlossen");
 
 %% Oszilloskop Kanaleinstellungen
+disp("Oszilloskopkanäle werden konfiguriert");
 % Benötigte Channel einschalten
 EnableDisableChannel(Oszi,Input_channel_nr,"on");
 EnableDisableChannel(Oszi,Output_channel_nr,"on");
@@ -60,9 +63,10 @@ pause(2);
 
 % Hier möglicherweise auch per-hand justierung von vert/horz scale falls
 % nötig
-
+disp("Konfiguration der Oszilloskopkanäle abgeschlossen");
 
 %% Oszilloskop Erfassungseinstellungen
+disp("Oszilloskop Erfassungseinstellungen werden konfiguriert");
 % Anzahl der aufzunehmenden Samples definieren
 write(Oszi,"ACQ:POIN "+80E6);
 % Erfassungsmodus auf High Resolution stellen
@@ -71,8 +75,9 @@ setAcquisitionType(Oszi,"HRES");
 setAcquisitionArithmetic(Oszi,"AVER");
 % Anzahl Samples pro Averaging auf 32 setzen
 setAcquisitionArithmeticAveragingCount(Oszi,32);
-
+disp("Konfiguration der Oszilloskop Erfassungseinstellungen abgeschlossen");
 %% Oszilloskop Triggereinstellungen
+disp("Oszilloskop Triggereinstellungen werden konfiguriert");
 % Triggermodus auf Automatisch setzen
 setTriggerMode(Oszi,"AUTO");
 % Triggertyp auf Edgetrigger setzen
@@ -87,23 +92,24 @@ setTriggerThresholdVoltage(Oszi,1,VPP_step/4)
 setEdgeTriggerCouplingMode(Oszi,"DC")
 % Trigger-Holdoff deaktivieren
 setTriggerHoldoffMode(Oszi,"OFF")
+disp("Konfiguration der Oszilloskop Triggereinstellungen abgeschlossen");
 
 %% Oszilloskop Math-einstellungen
+disp("Oszilloskop Mathchanneleinstellungen werden konfiguriert");
 % Benötigten Mathematikkanal einschalten
 EnableDisableMathChannel(Oszi,1,"ON");
 % Ableiten des DUT-Ausgangssignals
-defineMathChannelOperation(Oszi,1,DERI,"CH"+Output_channel_nr);
+defineMathChannelOperation(Oszi,1,"DERI","CH"+Output_channel_nr);
 % Pausieren um die Anzahl der Samples pro dx einzustellen
-pause;
 disp('EINSTELLEN DES ABLEITUNGSSAMPLE ANZAHL')
 disp('1. Auf dem Oszilloskopbildschirm auf den unten in der mitte angezeigten Math Kanal klicken')
 disp('2. In dem sich öffnenden Reiter den Punkt "Menu" auswählen und die Math-Kanal Einstellungen sollten sich öffnen')
 disp('3. Bei der ersten Gleichung, rechts neben "dx: XY Sa", auf den Knopf mit den 9 weißen Quadraten klicken')
 disp('4. Die Zahl 32 eingeben und mit dem "Enter" Knopf bestätigen')
 disp('5. Um das Skript fortzusetzen eine beliebige Taste in Matlab drücken')
+pause;
 % Pausieren um die Position und vertikale Skalierung der Math-Waveform per
 % Hand anzupassen
-pause;
 RunContinous(Oszi);
 disp('ANPASSEN DER POSITION UND VERTIKALEN SKALIERUNG DER MATH-WAVEFORM')
 disp('1. Möglicherweise überdeckt der "Equation Set Editor" die benötigten Felder. Diesen per Drag-and-Drop ein Stück nach links oben schieben')
@@ -112,32 +118,51 @@ disp('3. Mit diesen die Position und vertikale Skalierung der Ableitung einstell
 disp('3. Per Remote-Screen oder am direkt am Gerät auch über die Drehknöpfe einstellbar')
 disp('4. Hierbei den Oszilloskopbildschirm bestmöglich ausnutzen und die vertikale Skalierung so klein wie möglich einstellen, ohne das das Signal clippt')
 disp('5. Um das Skript fortzusetzen eine beliebige Taste in Matlab drücken')
-
-
+pause;
+StopAcquisition(Oszi);
+disp("Konfiguration der Oszilloskop Mathchanneleinstellungen abgeschlossen");
 %% Messdaten aufnehmen
-    RunSingleAcquisition(Oszi,32);
-    data_vector(1,i) = getQuickMeasurementData(Oszi,1); %Messen und speichern der Amplitude
-    disp("Amp"+i+": "+data_vector(1,i))
-    data_vector(2,i) = getQuickMeasurementData(Oszi,2); %Messen und speichern der Phase
-    disp("Phase"+i+": "+data_vector(2,i))   
-
-
+% Single Acquisition mit definierter Anzahl an zu erfassenden Waveforms 
+RunSingleAcquisition(Oszi,320);
+% Auslesen der Werte der Ableitungskurve
+math_channel_data = getMathChannelData(Oszi,1);
+% Auslesen der Oszilloskop Samplingrate
+fs = getSampleRate(Oszi);
+% Die ausgelesenen Werte an die Returnvariable übergeben
+output = {math_channel_data;fs};
 %% Messdaten auswerten
-if Auswertung == 1 
- 
+if Auswertung == 1
+% Zero-Padding der Math-channel Daten, um 1E6 Nullen
+    math_channel_data = [math_channel_data zeros(1,1000000)];
+% Bestimmung der Länge des Datenvektors    
+    L = length(math_channel_data);
+% Bestimmung der Anzahl an FFT-Punkten    
+    NFFT = 2^nextpow2(L);
+% Berechung des Frequenzvekors        
+    f = fs/2*linspace(0,1,NFFT/2+1);
+% Berechung der FFT und Skalierung mit 1/(Länge Datenvektor)    
+    Math_channel_data_FFT = fft(math_channel_data,NFFT)/L;
+% Extraktion von der Amplitudenwerten aus dem komplexen FFT-Vektor
+    abs_data = abs(Math_channel_data_FFT(1:NFFT/2+1));
+% Normierung der Amplitudenwerte, sodass im Bodediagramm die ersten Werte bei 0dB beginnen     
+    abs_data = abs_data ./ abs_data(1);
     
+% Subplot für den Amplitudengang
     subplot(2,1,1);
-    semilogx(freq_vector,20*log10(amp_vector));
+    semilogx(f,20*log10(abs_data))
+    xlim([1E6,50E6]);
     xlabel("Frequenz in [Hz]");
     ylabel("Amplitude in [dB]");
     title("Amplitudengang");
     
-    subplot(2,1,2); 
-    semilogx(freq_vector,phase_vector);
+% Subplot für den Amplitudengang    
+    subplot(2,1,2);
+    semilogx(f,unwrap(angle(Math_channel_data_FFT(1:NFFT/2+1))))
+    xlim([1E6,50E6]);
     xlabel("Frequenz in [Hz]");
     ylabel("Phase in [°]");
     title("Phasengang");
     
-    sgtitle("Frequenzgang des Filters");
+    sgtitle("Frequenzgang des Filters ");
 
 end
