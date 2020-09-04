50dc68055d9c503834bc87b7c931bfb2bc0d4396
Stefan Lammel <stefan.lammel@hs-augsburg.de>
2020-09-04 17:54:53 +0200

    refact: rework + doc

--- FrequenzgangAufnehmen.m
+++ FrequenzgangAufnehmen.m
@@ -1,130 +1,240 @@
+% ====================================================
+%> @brief Frequencyresponse acquisition script (Sinus Sweep)
+%> Operating principle:
+%> 1. Signalgenerator gets configured
+%> 2. Oscilloscope gets configured
+%> 3. Sinusoid stimulus gets sent twice with coupled signalgen. outputs
+%> -  One into the DUT
+%> -  One into one of the oscilloscope channels
+%> 4. For-loop over all frequency points
+%> 5. Single acquisition at each frequency point
+%> 6. Measuring the amplitude and phase of the DUT output signal with the
+%>    acquisition stopped
+%> 7. Saving the acquired data into a 2x1 data vector
+%> 8. Every 5 measurements the vertical and horizontal scaling gets
+%>    readjusted
+%> Optional:
+%> 9. Postprocessing of the acquired data in MATLAB
+%> 10. Plotting the frequencyresponse curves into two subplots
+%>
+%> @param SignalGen Instance of class DG4102
+%>
+%> @param Oszilloscope Selected channel RTM3000
+%>
+%> @param f_start Startfrequency for the sinusoid
+%>
+%> @param f_stop Stopfrequency for the sinusoid
+%>
+%> @param f_steps Number of evenly spaced frequencypoints between f_start
+%>                and f_stop
+%>
+%> @param VPP_initial Peak-to-Peak Amplitude for the sinusoid signal
+%>
+%> @param ChannelDUTInputSignal Oscilloscope channel which has no DUT in
+%>                              its signal path (e.G. "CH1")
+%>
+%> @param ChannelDUTOutputSignal Oscilloscope channel which has DUT in
+%>                               its signal path (e.G. "CH2")
+%>
+%> @param Auswertung Selectable boolean whether or not the data is getting
+%>                   postprocessed in MATLAB
+%> 
+%> @output output 2x1 double array
+%>         First row:  amplitude vector
+%>         Second row: phase vector
+% =====================================================
 
 
-function output = FrequenzgangAufnehmen(Gen,Oszi,f_start,f_stop,f_steps,VPP_initial,ChannelDUTInputSignal,ChannelDUTOutputSignal,Auswertung)
-%% Allgemeine Einstellungen und Variablen
+function output = FrequenzgangAufnehmen(SignalGen,Oszilloscope,f_start,f_stop,f_steps,VPP_initial,ChannelDUTInputSignal,ChannelDUTOutputSignal,Auswertung)
+%% Benötigte Variablen
 % Channelnummer aus Channelstring extrahieren
 Input_channel_nr = str2double(extractAfter(ChannelDUTInputSignal,"CH"));
 Output_channel_nr = str2double(extractAfter(ChannelDUTOutputSignal,"CH"));
 % Frequenzvektor definieren
 freq_vector = linspace(f_start,f_stop,f_steps);
+% Rundung der Frequenzwerte, um glatteres Signal erzeugen zu können
+round_freq = round(freq_vector,-3);
 % Datenvektor definieren
 data_vector = zeros(2,f_steps);
 % Benötigte zusätzliche Variablen definieren
-count = 0;
-VertMinReached = 0;
+measurementCount = 0;
+VertScaleMinReached = 0;
 
 %% Signalgeneratoreinstellungen
+disp("Signalgenerator wird konfiguriert");
+
 % Channelkopplung der Signalgeneratorausgänge aktivieren
-EnableDisableChannelCoupling(Gen,"ON");
+setSourceSinus(SignalGen,1,f_start,VPP_initial,0,0);
+setSourceSinus(SignalGen,2,f_start,VPP_initial,0,0);
+EnableDisableChannelCoupling(SignalGen,"ON");
 % Signaltyp und Anfangswerte definieren
 % Freq.  = Startfrequenz
 % Ampl.  = Anfangsamplitude
 % Offset = 0
 % Phase  = 0
-setSourceSinus(Gen,1,f_start,VPP_initial,0,0);
+setSourceSinus(SignalGen,1,f_start,VPP_initial,0,0);
+setSourceSinus(SignalGen,2,f_start,VPP_initial,0,0);
+disp("Konfiguration des Signalgenerators abgeschlossen");
 
-%% Oszilloskopeinstellungen
-% Measurement Plätze einschalten
-EnableDisableQuickMeasurement(Oszi,1,"ON");
-EnableDisableQuickMeasurement(Oszi,2,"ON");
-pause(0.5);
-% Measurement Typen einstellen
-defineQuickMeasurementType(Oszi,1,"PEAK");
-defineQuickMeasurementType(Oszi,2,"PHAS");
-pause(0.5);
-% Measurement Quellen definieren
-setQuickMeasurementSource(Oszi,1,ChannelDUTOutputSignal);
-setQuickMeasurementSource(Oszi,2,ChannelDUTInputSignal,ChannelDUTOutputSignal);
-pause(0.5);
+%% Oszilloskop Kanaleinstellungen
+disp("Oszilloskopkanäle werden konfiguriert");
+
 % Benötigte Channel einschalten
-EnableDisableChannel(Oszi,Input_channel_nr,"on");
-EnableDisableChannel(Oszi,Output_channel_nr,"on");
+EnableDisableChannel(Oszilloscope,Input_channel_nr,"on");
+EnableDisableChannel(Oszilloscope,Output_channel_nr,"on");
 pause(0.5);
 % Kopplung der Kanäle auf DC 50Ohm stellen
-setChannelCoupling(Oszi,Input_channel_nr,"DC");
-setChannelCoupling(Oszi,Output_channel_nr,"DC");
+setChannelCoupling(Oszilloscope,Input_channel_nr,"DC");
+setChannelCoupling(Oszilloscope,Output_channel_nr,"DC");
 pause(0.5);
 % Tastkopfeinheit auf Volt stellen
-setPassiveProbeMeasuringUnit(Oszi,Input_channel_nr,"V");
-setPassiveProbeMeasuringUnit(Oszi,Output_channel_nr,"V");
+setPassiveProbeMeasuringUnit(Oszilloscope,Input_channel_nr,"V");
+setPassiveProbeMeasuringUnit(Oszilloscope,Output_channel_nr,"V");
 pause(0.5);
 % Tastkopfdämpfung auf 1:1 stellen
-setPassiveProbeAttenuation(Oszi,Input_channel_nr,1);
-setPassiveProbeAttenuation(Oszi,Output_channel_nr,1);
+setPassiveProbeAttenuation(Oszilloscope,Input_channel_nr,1);
+setPassiveProbeAttenuation(Oszilloscope,Output_channel_nr,1);
 pause(0.5);
 % Vertikalen Offset auf 0 setzen
-setChannelOffset(Oszi,Input_channel_nr,0);
-setChannelOffset(Oszi,Output_channel_nr,0);
+setChannelOffset(Oszilloscope,Input_channel_nr,0);
+setChannelOffset(Oszilloscope,Output_channel_nr,0);
 pause(0.5);
 % Channelpolarität auf Normal setzen
-setChannelPolarity(Oszi,Input_channel_nr,"NORM");
-setChannelPolarity(Oszi,Output_channel_nr,"NORM");
+setChannelPolarity(Oszilloscope,Input_channel_nr,"NORM");
+setChannelPolarity(Oszilloscope,Output_channel_nr,"NORM");
 pause(0.5);
 % Channel Deskew time auf 0 setzen
-setChannelDeskew(Oszi,Input_channel_nr,0);
-setChannelDeskew(Oszi,Output_channel_nr,0);
+setChannelDeskew(Oszilloscope,Input_channel_nr,0);
+setChannelDeskew(Oszilloscope,Output_channel_nr,0);
 pause(0.5);
 % Channel ZOffset auf 0 setzen
-setChannelZOffset(Oszi,Input_channel_nr,0);
-setChannelZOffset(Oszi,Output_channel_nr,0);
+setChannelZOffset(Oszilloscope,Input_channel_nr,0);
+setChannelZOffset(Oszilloscope,Output_channel_nr,0);
 pause(0.5);
 % Vertikale Skalierung anpassen
-setVerticalScale(Oszi,Input_channel_nr,VPP_initial/5);
-setVerticalScale(Oszi,Output_channel_nr,VPP_initial/5);
+setVerticalScale(Oszilloscope,Input_channel_nr,VPP_initial/5);
+setVerticalScale(Oszilloscope,Output_channel_nr,VPP_initial/5);
 pause(0.5);
 % Horzontale Skalierung anpassen
 HorzScal = 1/(freq_vector(1)*6);
-setHorizontalScale(Oszi,HorzScal);
-pause(2);
+setHorizontalScale(Oszilloscope,HorzScal);
+pause(0.5);
+
+disp("Konfiguration der Oszilloskopkanäle abgeschlossen");
+
+%% Oszilloskop Messplatzeinstellungen
+disp("Oszilloskop Messplätze werden konfiguriert");
+% Messplätze einschalten
+EnableDisableQuickMeasurement(Oszilloscope,1,"ON");
+EnableDisableQuickMeasurement(Oszilloscope,2,"ON");
+pause(0.5);
+% Messungen einstellen
+defineQuickMeasurementType(Oszilloscope,1,"PEAK");
+defineQuickMeasurementType(Oszilloscope,2,"PHAS");
+pause(0.5);
+% Quellen der Messungen definieren
+setQuickMeasurementSource(Oszilloscope,1,ChannelDUTOutputSignal);
+setQuickMeasurementSource(Oszilloscope,2,ChannelDUTInputSignal,ChannelDUTOutputSignal);
+pause(0.5);
+
+disp("Konfiguration der Oszilloskop Messplätze abgeschlossen");
+
+%% Oszilloskop Erfassungseinstellungen
+disp("Oszilloskop Erfassungseinstellungen werden konfiguriert");
+
+% Anzahl der aufzunehmenden Samples definieren
+write(Oszilloscope,"ACQ:POIN "+80E6);
+% Erfassungsmodus auf Sample stellen
+setAcquisitionType(Oszilloscope,"SAMP");
+% Erfassungsarithmetik ausschalten
+setAcquisitionArithmetic(Oszilloscope,"OFF");
+
+disp("Konfiguration der Oszilloskop Erfassungseinstellungen abgeschlossen");
+
+%% Oszilloskop Triggereinstellungen
+disp("Oszilloskop Triggereinstellungen werden konfiguriert");
+
+% Triggermodus auf Automatisch setzen
+setTriggerMode(Oszilloscope,"AUTO");
+% Triggertyp auf Edgetrigger setzen
+setTriggerType(Oszilloscope,"EDGE");
+% Triggerquelle auf DUT-Eingangschannel setzen
+setTriggerSource(Oszilloscope,"CH"+Input_channel_nr);
+% Triggerflanke auf auf steigende Flanke setzen
+setEdgeTriggerSlope(Oszilloscope,"POS");
+% Triggerschwelle auf Nulldurchgang setzen
+setTriggerThresholdVoltage(Oszilloscope,1,0);
+% Triggerkopplung auf DC setzen
+setEdgeTriggerCouplingMode(Oszilloscope,"DC");
+% Trigger-Holdoff deaktivieren
+setTriggerHoldoffMode(Oszilloscope,"OFF");
+% Triggerposition auf 0 setzen
+setTriggerOffset(Oszilloscope,0);
+
+disp("Konfiguration der Oszilloskop Triggereinstellungen abgeschlossen");
 
 %% Messdaten aufnehmen
-for i = 1:1:f_steps % Schleife über alle Frequenzpunkte
-    RunSingleAcquisition(Oszi,1);
-    data_vector(1,i) = getQuickMeasurementData(Oszi,1); %Messen und speichern der Amplitude
-    disp("Amp"+i+": "+data_vector(1,i))
-    data_vector(2,i) = getQuickMeasurementData(Oszi,2); %Messen und speichern der Phase
-    disp("Phase"+i+": "+data_vector(2,i))
-    if i+1 > f_steps %Abbruchbedigung wenn alle Frequenzpunkte durchlaufen sind
-        output = {data_vector(1,:);data_vector(2,:)};
+disp("Messdaten werden aufgenommen");
+
+% Schleife über alle Frequenzpunkte
+for i = 1:1:f_steps 
+    RunSingleAcquisition(Oszilloscope,1);
+    % Messen und speichern der Amplitude
+    data_vector(1,i) = getQuickMeasurementData(Oszilloscope,1); 
+    % Messen und speichern der Phase
+    data_vector(2,i) = getQuickMeasurementData(Oszilloscope,2);
+    % Statusanzeiger
+    disp(i+"/"+f_steps+" complete");
+    % Abbruchbedigung, wenn alle Frequenzpunkte durchlaufen sind
+    if i+1 > f_steps
+        % Übergabe des Datenvektors and die Ausgabevariable
+        output = data_vector;
         break;
     else
-        round_freq = round(freq_vector(i+1),-3);
-        setFrequency(Gen,1,round_freq);
+    % Nächsten Frequenzpunkt einstellen
+    setFrequency(SignalGen,1,round_freq(i+1));
     end
 % Anpassung der vert. und horz. Skalierung nach jeder 5. Messung
-    if count >= 5
+    if measurementCount >= 5
         VertScal = data_vector(1,i)/7;
-        if VertMinReached ~= 1
+        if VertScaleMinReached ~= 1
             if VertScal > 500E-6 
-                setVerticalScale(Oszi,Output_channel_nr,VertScal);
+                setVerticalScale(Oszilloscope,Output_channel_nr,VertScal);
             else 
-                setVerticalScale(Oszi,Output_channel_nr,500E-6);
-                VertMinReached = 1;
+                setVerticalScale(Oszilloscope,Output_channel_nr,500E-6);
+                VertScaleMinReached = 1;
             end
         end
-        setHorizontalScale(Oszi,1/(freq_vector(i)*6));
-        count = 0;
+        setHorizontalScale(Oszilloscope,1/(freq_vector(i)*6));
+        measurementCount = 0;
     else
-        count = count+1;
+        measurementCount = measurementCount+1;
     end
     
 end
 
+disp("Messdatenaufnahme abgeschlossen");
+
 %% Messdaten auswerten
 if Auswertung == 1 
+    disp("Messdaten werden ausgewertet");
     %Vpp in Amplitudenwert umrechnen
-    amp_vector = 0.5 .* data_vector(1,:);
+    amp_vector = data_vector(1,:) ./ data_vector(1,1);
     %Phasensprünge entfernen
     phase_vector = -180/pi .* (unwrap(pi/180 .* data_vector(2,:))); 
     
+    % Subplot für den Amplitudengang
     subplot(2,1,1);
     semilogx(freq_vector,20*log10(amp_vector));
+    xlim([f_start,f_stop]);
     xlabel("Frequenz in [Hz]");
     ylabel("Amplitude in [dB]");
     title("Amplitudengang");
     
+    % Subplot für den Phasengang
     subplot(2,1,2); 
     semilogx(freq_vector,phase_vector);
+    xlim([f_start,f_stop]);
     xlabel("Frequenz in [Hz]");
     ylabel("Phase in [°]");
     title("Phasengang");
