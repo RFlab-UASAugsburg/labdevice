f14a8a73cc96999a8209b23b5ddbf280bbcf541b
Stefan Lammel <stefan.lammel@hs-augsburg.de>
2020-10-07 15:06:24 +0200

    feat: Testscript version 2

--- MaskentestDurchfuehren.m
+++ MaskentestDurchfuehren.m
@@ -33,17 +33,19 @@
 %>
 % =====================================================
 
-function output = MaskentestDurchfuehren(SignalGen,Oscilloscope,freq,Vpp_Rect,SigIn,SyncIn,AnzahlDatenPunkte,Auswertung,varargin)
+function output = MaskentestDurchfuehren(SignalGen,Oscilloscope,Vpp_Rect,SigIn,SyncIn,AnzahlDatenPunkte,Auswertung)
 %% Benötigte Variablen
 % Channelnummer aus Channelstring extrahieren
 Input_channel_nr = str2double(extractAfter(SigIn,"CH"));
 Sync_channel_nr = str2double(extractAfter(SyncIn,"CH"));
-% Amplitudenvektor definieren
-AmpVector = logspace(log10((Vpp_Rect/2)-0.2),log10((Vpp_Rect/2)-0.001),AnzahlDatenPunkte);
-
+% Amplitudenvektor definieren (in VPP)
+AmpVector = linspace((Vpp_Rect)-2,Vpp_Rect-0.05,AnzahlDatenPunkte);
+% Amplitudenvektor auf vier Nachkommastellen runden, da Matlabs genau berechnete Werte nicht genau
+% eingestellt werden können
+AmpVector = round(AmpVector,4);
 % Datenvektor definieren
 LengthAmpVector = length(AmpVector);
-DataVector = zeros(2,LengthAmpVector);
+CountVector = zeros(2,LengthAmpVector);
 
 %% Signalgeneratoreinstellungen
 disp("Signalgenerator wird konfiguriert");
@@ -56,8 +58,13 @@ disp("Signalgenerator wird konfiguriert");
 % 2.Typ    = Rauschen
 % 2.Ampl.  = 2Vpp
 % 2.Offset = 0V
-setSourceSquare(SignalGen,1,freq,Vpp_Rect,Vpp_Rect/2,0);
-write(SignalGen,":SOUR2:APPL:NOIS 2,0");
+setSourceSquare(SignalGen,2,1E3,Vpp_Rect,0,0);
+%write(SignalGen,":SOUR1:APPL:NOIS 2,0");
+write(SignalGen,":SOUR1:APPL:NOIS 4,0");
+% Sync Signal des zweiten Kanals einschalten (Kanal mit Rechtecksignal)
+write(SignalGen,":OUTP2:SYNC ON");
+% Polarität des Sync Signals auf posititv setzen
+write(SignalGen,":OUTP2:SYNC:POL POS");
 
 disp("Konfiguration des Signalgenerators abgeschlossen");
 
@@ -66,8 +73,8 @@ disp("Oszilloskopkanäle werden konfiguriert");
 % Benötigte Channel einschalten
 EnableDisableChannel(Oscilloscope,Input_channel_nr,"on");
 EnableDisableChannel(Oscilloscope,Sync_channel_nr,"on");
-% Kopplung der Kanäle auf DC 50Ohm stellen
-setChannelCoupling(Oscilloscope,Input_channel_nr,"DC");
+% Kopplung der Kanäle auf DC 1MOhm(Signalkanal) und DC 50Ohm(SyncKanal) stellen
+setChannelCoupling(Oscilloscope,Input_channel_nr,"DCL");
 setChannelCoupling(Oscilloscope,Sync_channel_nr,"DC");
 % Tastkopfeinheit auf Volt stellen
 setPassiveProbeMeasuringUnit(Oscilloscope,Input_channel_nr,"V");
@@ -88,8 +95,11 @@ setChannelDeskew(Oscilloscope,Sync_channel_nr,0);
 setChannelZOffset(Oscilloscope,Input_channel_nr,0);
 setChannelZOffset(Oscilloscope,Sync_channel_nr,0);
 % Vertikale Skalierung anpassen
-setVerticalScale(Oscilloscope,Input_channel_nr,115E-3);
-setVerticalScale(Oscilloscope,Sync_channel_nr,1); %%Sync Signal 3,3Vpp ?
+% 2V Rauschen
+%setVerticalScale(Oscilloscope,Input_channel_nr,250E-3); 
+% 4V Rauschen
+setVerticalScale(Oscilloscope,Input_channel_nr,500E-3); 
+setVerticalScale(Oscilloscope,Sync_channel_nr,500E-3); %%Sync Signal 3,3Vpp ?
 % Horzontale Skalierung anpassen
 setHorizontalScale(Oscilloscope,50E-6);
 
@@ -99,7 +109,7 @@ disp("Konfiguration der Oszilloskopkanäle abgeschlossen");
 disp("Oszilloskop Erfassungseinstellungen werden konfiguriert");
 
 % Anzahl der aufzunehmenden Samples definieren
-write(Oscilloscope,"ACQ:POIN "+80E6);
+write(Oscilloscope,"ACQ:POIN "+40E6);
 % Erfassungsmodus auf Sampling stellen
 setAcquisitionType(Oscilloscope,"SAMP");
 % Erfassungsarithmetik ausschalten
@@ -119,13 +129,13 @@ setTriggerSource(Oscilloscope,"CH"+Sync_channel_nr);
 % Triggerflanke auf auf steigende Flanke setzen
 setEdgeTriggerSlope(Oscilloscope,"POS")
 % Triggerschwelle auf 0V setzen
-setTriggerThresholdVoltage(Oscilloscope,1,0);
+setTriggerThresholdVoltage(Oscilloscope,1,750E-3);
 % Triggerkopplung auf DC setzen
 setEdgeTriggerCouplingMode(Oscilloscope,"DC");
 % Trigger-Holdoff deaktivieren
 setTriggerHoldoffMode(Oscilloscope,"OFF");
 % Triggerposition eine Division vom rechten Bildschirmrand
-setTriggerOffset(Oscilloscope,250E-6); 
+setTriggerOffset(Oscilloscope,-250E-6); 
 
 disp("Konfiguration der Oszilloskop Triggereinstellungen abgeschlossen");
 
@@ -136,76 +146,103 @@ EnableDisableMaskingTest(Oscilloscope, "ON");
 % Maskenquelle einstellen
 setMaskTestSource(Oscilloscope, "CH"+Input_channel_nr);
 % Bitfehlermaske laden
-write(Oszi,'MMEM:CDIR "/INT/REFERENCE"');
-write(Oszi,'MASK:LOAD "BER.MSK"');
+write(Oscilloscope,'MMEM:CDIR "/INT/REFERENCE"');
+write(Oscilloscope,'MASK:LOAD "BER.MSK"');
 % Maskenfehleraktion definieren
 %   Deaktivieren: Sound,Screenshot,Save Waveformdata,Puls
 %   Aktivieren: Stop
-write(SignalGen,"MASK:ACT:SOUN:EVEN:MODE OFF");
-write(SignalGen,"MASK:ACT:SCRS:EVEN:MODE OFF");
-write(SignalGen,"MASK:ACT:WFMS:EVEN:MODE OFF");
-write(SignalGen,"MASK:ACT:PULS:EVEN:MODE OFF");
-write(SignalGen,"MASK:ACT:STOP:EVEN:MODE EACH");
+write(Oscilloscope,"MASK:ACT:SOUN:EVEN:MODE OFF");
+write(Oscilloscope,"MASK:ACT:SCRS:EVEN:MODE OFF");
+write(Oscilloscope,"MASK:ACT:WFMS:EVEN:MODE OFF");
+write(Oscilloscope,"MASK:ACT:PULS:EVEN:MODE OFF");
+write(Oscilloscope,"MASK:ACT:STOP:EVEN:MODE EACH");
 % Anzahl Maskenverletzungen auf 100 stellen
-write(SignalGen,"MASK:ACT:STOP:EVEN:COUN 100");
-% Run Continous 
-RunContinous(Oscilloscope);
-% Erfassungsmodus auf Sampling stellen, da der Maskentest Envelope
-% automatisch einstellt
-setAcquisitionType(Oscilloscope,"SAMP");
+write(Oscilloscope,"MASK:ACT:STOP:EVEN:COUN 100");
+
 
 disp("Konfiguration des Maskentests abgeschlossen");
+
+
 %% Messdaten aufnehmen
+% Schleife über jeden Wert in dem oben definierten Amplitudenvektor
 for i = 1:1:LengthAmpVector
     % Amplitude einstellen
-    setAmplitude(SignalGen,1,AmpVector(i));
-    % Fehlerzähler zurücksetzen
+    setAmplitude(SignalGen,2,AmpVector(i));
+    % Maskentestanwendung stoppen
+    stopMaskTest(Oscilloscope);
+    % Maskentestzähler zurücksetzen
     resetMaskCounter(Oscilloscope);
-    % Run Continous da nach Maskenstopbedingung Erfassung beendet wird
-    % Startet den nächsten Maskentest automatisch
+    % Erfassung des Oszilloskops auf "RUN" stellen
     RunContinous(Oscilloscope);
-    % Maskentest starten (Beim ersten Durchlauf)
+    % Erfassungstyp wieder auf "Sampling" setzen, da Maskentest dies
+    % automatisch auf Envelope(Hüllkurve) setzt
+    setAcquisitionType(Oscilloscope,"SAMP");
+    % Maskentestanwendung starten
+    if i == 40 
+        % Anzahl Maskenverletzungen auf 20 stellen
+        write(Oscilloscope,"MASK:ACT:STOP:EVEN:COUN 50");
+    end
     runMaskTest(Oscilloscope);
-    % Maskenfehlersamples und Totalsamples auslesen
-    DataVector(1,i) = getTotalMaskTestCount(Oscilloscope);
-    DataVector(2,i) = getTotalFailedMaskTestCount(Oscilloscope);
+    % Kurze Pause bevor erster Wert übermittelt wird, da sonst der
+    % Signalgenerator nicht antwortet
+    pause(1);
+    % Der laufende Maskentest zählt nicht als Befehl, welcher über die
+    % integrierte "Operation Complete(OPC)" oder "Wait to continue(WAI)"
+    % synchronisiert werden kann. Während die Messung läuft(bis Fehlerzahl
+    % errreicht ist) zählt der Befehl zum Starten der Messung schon als
+    % abgeschlossen. 
+    % Deswegen wird zweimal, mit zwei Sekunden Verzögerung, der aktuelle 
+    % Totalcount der Messung aufgenommen. Wenn beide Werte gleich sind, ist
+    % die Messung beendet.
+    while 1
+    compareCount1 = getTotalMaskTestCount(Oscilloscope);
+    pause(2);
+%     if i > 40
+%         pause(2);
+%     end
+    compareCount2 = getTotalMaskTestCount(Oscilloscope);
+    if compareCount1 == compareCount2
+        break;
+    end
+    end
+    % Totalcount des Maskentests ausgelesen
+    CountVector(1,i) = getTotalMaskTestCount(Oscilloscope);
+    CountVector(2,i) = getTotalFailedMaskTestCount(Oscilloscope);
+    
+    
+    disp("Messung: "+i)
+    
+    
+    % Wenn alle Amplituden durchlaufen wurden, wird die Messungschleife beendet
     if i+1 > LengthAmpVector
         % Übergabe des Datenvektors und des Amplitudenvektors an die
         % Ausgabevariable
-        output = {"Amplitudenvektor","TotalCountMaskTest","FailedCountMaskTest"; ...
-            AmpVector,DataVector(1,:),DataVector(2,:)};
+        output = {"Amplitudenvektor","TotalCountMaskTest","TotalFailedCountMaskTest"; ...
+                  AmpVector,CountVector(1,:),CountVector(2,:)};
         break;
     end
-    
-    
+
 end
 
 
 %% Messdaten auswerten
 if Auswertung == 1
-    %N0 definieren
+    % N0 definieren
     % 0.1374 bei 2Vpp Rauschen
     % 0.4910 bei 4Vpp Rauschen
     N0 = 0.1374;
-    %Amplitudenvektor in Leistungsvektor umrechnen
-    PowerVector = (AmpVector ./2) .^2;
-    
-    %SNR-Vektor berechnen
-    SNR_Vector = 10*log(PowerVector ./ N0);
-    
+    % Amplitudenvektor in Leistungsvektor umrechnen
+    PowerVector = (AmpVector ./2) .^2;  
+    % SNR-Vektor berechnen
+    SNR_Vector = 10.*log(PowerVector ./ N0);
+    % BER berechnen
+    BER_Vector = CountVector(2,:) ./ CountVector(1,:);
+    % Bitfehlerkurve plotten
+    semilogy(SNR_Vector,BER_Vector);
+    xlabel("SNR in dB");
+    ylabel("BER");
 
 end
-
-
-
-
-
-
-
-
-
-
-
 
 
 end
